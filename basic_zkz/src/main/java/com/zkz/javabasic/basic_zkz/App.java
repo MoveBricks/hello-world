f00d5ff8c5d3e3b32a71d5e9f3499b614857a2d0
sunny <2449393804@qq.com>
2018-01-06 15:38:05 +0800

    java基础练习工程

---
+++ basic_zkz/src/main/java/com/zkz/javabasic/basic_zkz/App.java
@@ -0,0 +1,13 @@
+package com.zkz.javabasic.basic_zkz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
